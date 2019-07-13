bf9cf8b4525922a2cc9ee652bbd2c4fa52b787bd
David R. Myers <davidrmyersii@gmail.com>
2019-07-13 12:47:52 -0400

    Add ability to mark tasks as completed

--- vue-todo/app.js
+++ vue-todo/app.js
@@ -2,29 +2,29 @@ var app = new Vue({
   el: '#app',
 
   data: {
-    tasks: [
-      {
-        name: 'Take out the trash',
-        is_completed: false,
-        is_deleted: false,
-      },
-    ],
-    new_task: ''
+    tasks: [],
+    new_task: {
+      name: '',
+      is_completed: false,
+    },
   },
   computed: {
     available_tasks() {
       return this.tasks.filter(task => !task.is_deleted);
-    },   
+    },
   },
   methods: {
     add_task() {
       var task  =  {
-        is_completed : false,
+        name: this.new_task.name,
+        is_completed : this.new_task.is_completed,
         is_deleted: false,
-        name: this.new_task
-      }
+      };
+
       this.tasks.push(task);
-      this.new_task = '';
+
+      this.new_task.name = '';
+      this.new_task.is_completed = false;
     }
   }
 });
